a9752aafb5da8fedc9e94ffb342c9caa560e9fbd
Olesya <olesikand@mail.ru>
2023-02-18 20:03:04 +0300

    deploy3

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -140,8 +140,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static_backend/' 
+if DEBUG: 
+    STATICFILES_DIRS = [ 
+        os.path.join(BASE_DIR, 'static_backend'), 
+    ] 
+else: 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static_backend')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
