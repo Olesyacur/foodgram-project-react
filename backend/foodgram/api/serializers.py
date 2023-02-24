e3eb9074f1724f18bd1001ad00efe0a351521bec
Olesya <olesikand@mail.ru>
2023-02-24 11:54:54 +0300

    rules

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -50,7 +50,7 @@ class UserCustomSerializer(UserSerializer):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
-        return Follow.objects.filter( 
+        return Follow.objects.filter(
             user=user, author=obj).exists()
     # def get_is_subscribed(self, obj):
     #     request = self.context.get('request')
